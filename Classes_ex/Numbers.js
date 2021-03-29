fbc91ac279bb595a212af5db17038807502158ce
Priya-raja <priya.thevar89@gmail.com>
2021-03-29 14:33:48 +0530

    destructuring

--- Classes_ex/Numbers.js
+++ Classes_ex/Numbers.js
@@ -4,7 +4,7 @@ const newNumbers = [...numbers,5];
 console.log(newNumbers);
 
 const person = {
-    name: 'priya'
+    name: 'MaxMiller'
 };
 
 const newPerson = {
@@ -17,3 +17,12 @@ const filter = (...args) => {
     return args.filter(el => el === 1);
 }
 console.log(filter(1,2,3));
+
+//Array destructuring
+
+const [num1, ,num3] = numbers;
+
+console.log(num1,num3);
+
+const {name1, age} = {name1:'Willow', age: 27};
+console.log(name1);
