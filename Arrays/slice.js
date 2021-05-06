50b331385119cdd675e1ecbef19a8327a3d62abe
Priya-raja <priya.thevar89@gmail.com>
2021-05-06 13:59:50 +0530

    slice method

---
+++ Arrays/slice.js
@@ -0,0 +1,5 @@
+var users = ["Ted","sam","Tom","Sor","Lora"];
+//  console.log(users.slice(1));
+//  console.log(users.slice(1,4)); //includes the 1st and excludes the last one
+ console.log(users.splice(1,3, "Hi")); //replace from 1 to 3 places
+ console.log(users);
