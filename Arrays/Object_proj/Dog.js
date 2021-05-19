eaf95c65216c5d8b99d9a75f34ecbd412de22a3e
Priya-raja <priya.thevar89@gmail.com>
2021-05-19 23:28:54 +0400

    Oject access

---
+++ Arrays/Object_proj/Dog.js
@@ -0,0 +1,19 @@
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+console.log(ourDog.name)
+  // Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line;
+  
+  var playerNumber = 16;       // Change this Line
+  var player = testObj[playerNumber]
+  console.log(player)
